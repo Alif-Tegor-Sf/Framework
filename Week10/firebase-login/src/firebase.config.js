766addb96cf4887718a05d955453e6e020eba37a
Alif-Tegor-Sf <71006002+Alif-Tegor-Sf@users.noreply.github.com>
2022-06-27 13:24:10 +0700

    Pertemuan 10

---
+++ Week10/firebase-login/src/firebase.config.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyBlRLyjnllKy-ccKHkIHe-lbtdhhL4bjwk",
+    authDomain: "uas-pbf-aaf62.firebaseapp.com",
+    databaseURL: "https://uas-pbf-aaf62-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "uas-pbf-aaf62",
+    storageBucket: "uas-pbf-aaf62.appspot.com",
+    messagingSenderId: "9418964792",
+    appId: "1:9418964792:web:3301f3bdd9bff1856ecbcd",
+    measurementId: "G-LKMS0FESME"
+  };n
